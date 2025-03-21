f394bb475a3671508b292f682004d5e7b3b1f15a
Jean-Claude AZIAHA <aziaha.formations@gmail.com>
2025-03-21 17:38:33 +0100

    add: Mise à jour de l'entête de la page

--- public/index.php
+++ public/index.php
@@ -1,9 +1,22 @@
+<?php
+    $title = "Accueil";
+    $description = "Liste des films";
+    $keywords = "accueil, liste, films";
+    $fontAwesome = <<<HTML
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+HTML;
+?>
 <?php require __DIR__ . "/../partials/head.php"; ?>
 
     <?php require __DIR__ . "/../partials/nav.php"; ?>
 
     <main>
-        <h1>Hello World!</h1>
+        <h1 class="text-center my-3 display-5">Liste des films</h1>
+        <div class="container">
+            <div class="d-flex justify-content-end align-items-center my-3">
+                <a href="" class="btn btn-primary shadow"><i class="fa-solid fa-plus"></i> Nouveau film</a>
+            </div>
+        </div>
     </main>
 
     <?php require __DIR__ . "/../partials/footer.php"; ?>
